6c0237b7f2e2ec4a3d687d4b1f24fd2ee1d342ec
mrcoder991 <udaygirhepunje41@gmail.com>
2022-03-23 23:25:57 +0530

    created basic routes

--- backend/index.js
+++ backend/index.js
@@ -1,16 +1,14 @@
 const connectToMongo = require('./db.js');
 const express = require('express')
 
-
-
 connectToMongo();
-
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+//Avialable routes
+app.use('/api/auth', require('./routes/auth.js'));
+app.use('/api/notes', require('./routes/notes'));
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port http://localhost:${port}`)
