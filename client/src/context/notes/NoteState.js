ce3b6ec20588457ec20206b2c9fe695a37e78b3f
mrcoder991 <udaygirhepunje41@gmail.com>
2022-04-17 21:45:32 +0530

    port no

--- client/src/context/notes/NoteState.js
+++ client/src/context/notes/NoteState.js
@@ -2,7 +2,7 @@ import NoteContext from "./noteContext";
 import { useState } from "react";
 
 const NoteState = (props) => {
-  const host = "http://localhost:5000"
+  const host = process.env.PORT
   const notesInitial = []
 
   const authToken = localStorage.getItem('token')
